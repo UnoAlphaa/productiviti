accd19e5a4beae1aae2ab089583d69dbecc9bee2
UnoCodes <asieluecharlie@gmail.com>
2026-03-02 19:33:10 +0100

    fix: correct query key in useProducts hook to ensure proper data fetching

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -3,7 +3,7 @@ import {createProduct, getAllProducts} from "../lib/api"
 
 
 export const useProducts =  () => {
-    const result = useQuery({querykey:["production"], queryFn:getAllProducts, initialData : []});
+    const result = useQuery({querykey:["products"], queryFn:getAllProducts});
     return result;
 }
 
